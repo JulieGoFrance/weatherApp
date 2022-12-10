cf068bb727019c3646389878fe84cb13605123f4
JulieGoFrance <juliego2019@gmail.com>
2022-12-10 13:20:52 +0100

    premiere partie Vues Finalisees

--- weatherDemo/Views/WelcomeView.swift
+++ weatherDemo/Views/WelcomeView.swift
@@ -6,11 +6,22 @@
 //
 
 import SwiftUI
-
+import CoreLocation
+import CoreLocationUI
 struct WelcomeView: View {
     @EnvironmentObject var locationManager : LocationManager
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            VStack(spacing : 20){
+                Text("Welcome to the weather App").bold().font(.title)
+                Text(" donnez votre localisation pour avoir la météo dans votre zone").padding()
+            }.multilineTextAlignment(.center).padding()
+            LocationButton(.shareCurrentLocation){
+                locationManager.requestLocation()
+            }.foregroundColor(.white)
+            
+        }.frame(maxWidth : .infinity, maxHeight: .infinity)
     }
 }
 
